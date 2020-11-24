48ebc66437f9a59f3fc6d6e8ece318ed291aaad7
art.dambrine <art.dambrine@gmail.com>
2020-11-24 23:06:46 +0100

    [Test] /ordinateur/{id}

---
+++ src/main/java/servlets/OrdinateurFiche.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "OrdinateurFiche", urlPatterns = "/ordinateur/*")
+public class OrdinateurFiche extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        String name = request.getPathInfo().substring(1);
+
+        PrintWriter out = response.getWriter();
+        out.print("<h1>Hello " + name + "</h1>");
+    }
+}
